04d9006368e9c8221c1107202ea0f78d9939dab2
A. H. M. Faisal <ahmfaisal10@gmail.com>
2026-07-11 12:44:41 +0600

    chore: routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
+import { adminRoutes } from '@/modules/admin/admin.routes';
 import { authRoutes } from '@/modules/auth/auth.routes';
+import { gearController } from '@/modules/gear/gear.controller';
+import { gearRoutes } from '@/modules/gear/gear.routes';
 import { healthRoutes } from '@/modules/healths/healths.routes';
+import { paymentRouter } from '@/modules/payments/payment.routes';
+import { providerRoutes } from '@/modules/provider/provider.routes';
+import { rentalRoutes } from '@/modules/rental/rental.routes';
+import { reviewRoutes } from '@/modules/review/review.routes';
 
 type Route = {
 	path: string;
 	router: Router;
 };
+
+const categoryRouter = Router();
+categoryRouter.get('/', gearController.getCategories);
 
 const routes: Route[] = [
 	{
@@ -15,6 +25,34 @@ const routes: Route[] = [
 	{
 		path: '/auth',
 		router: authRoutes,
+	},
+	{
+		path: '/gear',
+		router: gearRoutes,
+	},
+	{
+		path: '/categories',
+		router: categoryRouter,
+	},
+	{
+		path: '/provider',
+		router: providerRoutes,
+	},
+	{
+		path: '/admin',
+		router: adminRoutes,
+	},
+	{
+		path: '/rentals',
+		router: rentalRoutes,
+	},
+	{
+		path: '/reviews',
+		router: reviewRoutes,
+	},
+	{
+		path: '/payments',
+		router: paymentRouter,
 	},
 ];
 
